c95403d9b91b3f8baf96ab91e1d9448eb9653ac0
unknown <silvanovaldez53@outlook.com>
2022-07-05 12:36:57 -0500

    pwa

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import {BrowserRouter} from "react-router-dom";
 import { store, persistor } from './redux/store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import * as serviceWorker from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -22,4 +23,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorker.register();
